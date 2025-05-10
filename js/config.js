75f4073f0d37e4f8774673b4233663c3401819ea
rahul25-25 <rahulyadavgangavennolla@gmail.com>
2025-05-10 13:24:19 -0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-2_pEUc4HFUM', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '38ktso501478nlemir3e9qs9j4', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
