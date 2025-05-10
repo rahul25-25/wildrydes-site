dd610a382cbfd1e20193413164f3e460d968c225
rahul25-25 <rahulyadavgangavennolla@gmail.com>
2025-05-10 13:40:09 -0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-2_pEUc4HFUM', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '38ktso501478nlemir3e9qs9j4', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: '', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-2' // e.g. us-east-2
     },
     api: {
